af2fcd7b1aa0b4bba121ec4ddc298681f529e5fe
Chacha Sikes <chachasikes@gmail.com>
2019-04-11 03:48:00 -0700

    adjust display settings for quill scene in hand painted skybox :D

--- examples/quill_scenes.js
+++ examples/quill_scenes.js
@@ -31,11 +31,11 @@ export let mything = {
 		{
 			name:"camera",
 			camera:{
-				position:{x:20,y:5,z:50},
-				lookat:{x:-2,y:2,z:-2}
+        position:{x:20,y:5,z:50},
+				lookat:{x:0,y:0,z:0},
 			},
 			orbit:{
-				lookat:{x:0,y:10,z:0},
+				lookat:{x:10,y:10,z:10},
 			}
 		},
 
@@ -45,43 +45,44 @@ export let mything = {
 			// a behavior on the blob; in this case a 3js light - maps to a class named BehaviorLight
 			light:{
 				// a property of the behavior - simply used by the behavior at will or thrown away
-				position:{x:-30,y:40,z:-50},
+				position:{x:-50,y:-50,z:-50},
 				color:0xFFFFFF,
+        castShadow: false
 			}
 		},
 
 		// some ground
-		{
-			name:"ground",
-			mesh:{
-				art:"sphere",
-				position:{x:0,y:-8,z:0},
-				scale:{x:300,y:1,z:300},
-				color:0x270212,
-			}
-		},
+		// {
+		// 	name:"ground",
+		// 	mesh:{
+		// 		art:"sphere",
+		// 		position:{x:0,y:-8,z:0},
+		// 		scale:{x:300,y:1,z:300},
+		// 		color:0x270212,
+		// 	}
+		// },
 
 		// art
 		{
 			name:"scene_1",
 			mesh:{
-				art:"../art/quillustrations/forest_rough_sketch",
-				position:{x:2,y:2,z:2},
-				scale:{x:100000,y:100000,z:100000},
+				art:"/art/quillustrations/forest_rough_sketch",
+				position:{x:0,y:0,z:0},
+				scale:{x:10,y:10,z:10},
 				color:0x27ff12,
 			}
 		},
 
 		// art
-		{
-			name:"scene_2",
-			mesh:{
-				art:"../art/quillustrations/red_animated_sketch",
-				position:{x:-2,y:2,z:-2},
-				scale:{x:100000,y:100000,z:100000},
-				color:0xff0212,
-			}
-		},
+		// {
+		// 	name:"scene_2",
+		// 	mesh:{
+		// 		art:"../art/quillustrations/red_animated_sketch",
+		// 		position:{x:-2,y:2,z:-2},
+		// 		scale:{x:1000,y:1000,z:1000},
+		// 		color:0xff0212,
+		// 	}
+		// },
 
 	]
 }
